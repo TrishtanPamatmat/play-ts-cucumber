690d6cb22f9b4db90b8fd933d7e03ff78336fc03
Trishtan <trishtan.pamatmat@gmail.com>
2025-04-24 00:16:24 +0800

    Change report overwrite

--- report.js
+++ report.js
@@ -1,12 +1,15 @@
+// report.js
 const reporter = require('cucumber-html-reporter');
+const now = new Date().toISOString().replace(/[-:]/g, '').split('.')[0];
+const outputFilename = `cucumber-report-${now}.html`;
 
 const options = {
   theme: 'bootstrap',
   jsonFile: 'cucumber-report.json',
-  output: 'cucumber-report.html',
+  output: outputFilename,
   reportSuiteAsScenarios: true,
   scenarioTimestamp: true,
-  launchReport: true,
+  launchReport: true, // Prevent automatic opening if you're generating multiple reports
 };
 
 reporter.generate(options);
